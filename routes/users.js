d440b1277882873f498b2594e62fd85aa266007a
Alexa Scott <arscott4792@gmail.com>
2018-04-02 13:12:40 -0700

    updated post /users endpoint

--- routes/users.js
+++ routes/users.js
@@ -9,17 +9,29 @@ const User = require('../models/user');
 
 
 /* ========== POST/CREATE A USER ========== */
-router.post('/users', (req, res) => {
-  console.log('making post request to /api/users');
-
+router.post('/users', (req, res, next) => {
   const { fullname, username, password } = req.body;
-  
-  const newUser = {fullname, username, password};
-  User.create(newUser)
+  return User.hashPassword(password)
+    .then(digest => {
+      const newUser = {
+        username,
+        password: digest,
+        fullname
+      };
+      return User.create(newUser);
+    })
     .then(user => {
       return res.status(201).location(`${req.originalUrl}/${user.id}`).json(user);
+    })
+    .catch(err => {
+      if (err.code === 11000) {
+        err = new Error('The username already exists');
+        err.status = 400;
+      }
+      next(err);
     });
-});
+}); 
+
 
 
 
