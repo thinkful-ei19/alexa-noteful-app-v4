e1d340583625246d04f5f6e1f57f0248d028bc01
Alexa Scott <arscott4792@gmail.com>
2018-04-06 08:19:43 -0700

    added hooks

--- test/login.test.js
+++ test/login.test.js
@@ -14,5 +14,38 @@ const expect = chai.expect;
 
 chai.use(chaiHttp);
 
+describe('Noteful API - Users', function() {
+  let token;
+  const username = 'exampleUser';
+  const password = 'examplePass';
+  const fullname = 'Example User';
+
+  before(function () {
+    return mongoose.connect(TEST_MONGODB_URI)
+      .then(() => mongoose.connect.db.dropDatabase());
+  });
+
+  beforeEach(function () {
+    return User.create({
+      username,
+      password,
+      fullname
+    });
+  });
+
+  after(function () {
+    return mongoose.connection.db.dropDatabase();
+    // alternatively
+    // return User.remove();
+  });
+
+  after(function () {
+    return mongoose.disconnect();
+  });
+
+
+
+});
+
 
 
