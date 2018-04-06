2bfee5c8a9428eb47c7fc00c884ab42c3b875c02
Alexa Scott <arscott4792@gmail.com>
2018-04-06 09:29:13 -0700

    added test for Should reject request with no credentials

--- test/login.test.js
+++ test/login.test.js
@@ -10,17 +10,19 @@ const { TEST_MONGODB_URI, JWT_SECRET } = require('../config');
 
 const User = require('../models/user');
 
+const seedUsers = require('../db/seed/users');
+
 const expect = chai.expect;
 
 chai.use(chaiHttp);
 
 describe('Noteful API - Login', function() {
-  let token;
-  const username = 'exampleUser';
-  const password = 'password10';
-  const fullname = 'Example User';
-  const id = '333333333333333333333300';
-  const digest = '$2a$10$32jNGd6nmugrIgQl6ClTC.v.QRSHwMLC.XegKx7mnUolEDSaNu7y6';
+//   let token;
+//   const username = 'exampleUser';
+//   const password = 'password10';
+//   const fullname = 'Example User';
+//   const id = '333333333333333333333300';
+//   const digest = '$2a$10$32jNGd6nmugrIgQl6ClTC.v.QRSHwMLC.XegKx7mnUolEDSaNu7y6';
 
   before(function () {
     return mongoose.connect(TEST_MONGODB_URI)
@@ -28,15 +30,16 @@ describe('Noteful API - Login', function() {
   });
 
   beforeEach(function () {
-    return User.create({
-      _id: id,
-      username,
-      password: digest,
-      fullname
-    });
+    return User.insertMany(seedUsers);
+    // return User.create({
+    //   _id: id,
+    //   username,
+    //   password: digest,
+    //   fullname
+    // });
   });
 
-  after(function () {
+  afterEach(function () {
     return mongoose.connection.db.dropDatabase();
     // alternatively
     //return User.remove();
@@ -49,9 +52,10 @@ describe('Noteful API - Login', function() {
   describe('/api/login', function() {
     describe('POST', function () {
       it('Should return a valid auth token', function () {
+        const { _id: id, username, fullname } = seedUsers[0];
         return chai.request(app)
           .post('/api/login')
-          .send( {username, password} )
+          .send( {username, password: 'password10'} )
           .then(res => {
             expect(res).to.have.status(200);
             expect(res.body).to.be.a('object');
@@ -63,6 +67,18 @@ describe('Noteful API - Login', function() {
             expect(payload.user).to.deep.equal({ id, username, fullname });
           });
       }); 
+
+      it('Should reject request with no credentials', function() {
+        //const testUser = {};
+        return chai.request(app)
+          .post('/api/login')
+          .send()
+          .catch(err => err.response)
+          .then(res => {
+            expect(res).to.have.status(400);
+            expect(res.body.message).to.equal('Bad Request');
+          });
+      });
     });
   });
 
